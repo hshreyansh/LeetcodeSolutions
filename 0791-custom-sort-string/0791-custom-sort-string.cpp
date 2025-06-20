42930010908c237288c55a0c29c9ad59ba71dbc4
Shreyansh Shukla <shreyansh.maholi@gmail.com>
2025-06-20 23:09:47 +0530

    Time: 0 ms (100%), Space: 8.1 MB (97.73%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -1,31 +1,14 @@
 class Solution {
 public:
+    static string str;
+    static bool compare(char ch1, char ch2)
+    {
+        return(str.find(ch1)<str.find(ch2));
+    }
     string customSortString(string order, string s) {
-        int freq[26] = {0};
-        
-        // Count frequency of each character in 's'
-        for (char c : s) {
-            freq[c - 'a']++;
-        }
-
-        string result = "";
-
-        // Add characters from 'order' in the given order
-        for (char c : order) {
-            while (freq[c - 'a'] > 0) {
-                result += c;
-                freq[c - 'a']--;
-            }
-        }
-
-        // Add remaining characters that were not in 'order'
-        for (char c = 'a'; c <= 'z'; ++c) {
-            while (freq[c - 'a'] > 0) {
-                result += c;
-                freq[c - 'a']--;
-            }
-        }
-
-        return result;
+        str=order;
+        sort(s.begin(), s.end(), compare);
+        return s;
     }
 };
+string Solution::str;
