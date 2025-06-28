8311acae9d1a0043536d650232a34d5293f4c4bf
Shreyansh Shukla <shreyansh.maholi@gmail.com>
2025-06-28 14:11:30 +0530

    Time: 0 ms (100%), Space: 8.9 MB (80.38%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans="";
+        int count=0;
+        for( int i=0; i< s.size(); i++)
+        {
+            if(s[i]==')') count--;
+            if(count!=0)
+            {
+                ans.push_back(s[i]);
+            }
+            if(s[i]=='(') count++;
+        }
+        return ans;
+    }
+};
