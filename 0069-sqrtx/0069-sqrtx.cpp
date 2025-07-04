574bb15c34bb540f56d73458e1668c11397387d6
Shreyansh Shukla <shreyansh.maholi@gmail.com>
2025-07-05 00:38:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,9 +1,26 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int n;
-        n=sqrt(x);
-        return n;
-        
+        int s=0;
+        int n= x;
+        int e=x;
+        int mid=s+(e-s)/2;
+
+        while(s<=e){
+            mid=s+(e-s)/2;
+            if(mid*mid>n)
+            {
+                e=mid-1;
+            }
+            else if(mid*mid<n)
+            {
+                s=mid+1;
+            }
+            else if(mid*mid==n)
+            {
+                return mid;
+            }
+        }
+        return mid-1;
     }
 };
