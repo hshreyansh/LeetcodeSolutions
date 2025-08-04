343c89242215feff0bb589bd97b0e00dad1cc843
Shreyansh Shukla <shreyansh.maholi@gmail.com>
2025-08-04 16:58:07 +0530

    Time: 4 ms (44.44%), Space: 153.8 MB (44.44%) - LeetHub

--- 3638-maximum-balanced-shipments/3638-maximum-balanced-shipments.cpp
+++ 3638-maximum-balanced-shipments/3638-maximum-balanced-shipments.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int maxBalancedShipments(vector<int>& weight) {
+        int ans= 0;
         int n = weight.size();
-        int balanced = 0;
-        int j = 1;  
-        for (int i = 0; i < n; ) {
-            if (j <= i) j = i + 1;
-            if (j >= n) break;  
-            int big = *max_element(weight.begin() + i, weight.begin() + j);
-            if (weight[j] < big) {
-                balanced++;
-                i = j + 1;
-                j = i + 1;
-            } 
-            else
+        for( int i =0;i<n;i++)
+        {
+            int maxi = weight[i];
+            while( i< n )
             {
-                j++;
+                maxi = max( maxi, weight[i] );
+                if( weight[i]!= maxi)
+                {
+                    ans++;
+                    break;
+                }
+                i++;
             }
         }
-        return balanced;
+        return ans ;
     }
 };
