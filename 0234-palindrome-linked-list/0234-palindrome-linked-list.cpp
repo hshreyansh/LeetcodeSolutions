523a93202f8948cee154e07f86b55a6dadbc803c
Shreyansh Shukla <shreyansh.maholi@gmail.com>
2025-09-18 18:06:56 +0530

    Time: 0 ms (100%), Space: 118.1 MB (71.11%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,24 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
     bool isPalindrome(ListNode* head) {
-        vector<int> arr;
-
-        while (head != nullptr) {
-            arr.push_back(head->val);
-            head = head->next;
+        // Approach 1; 
+            // create a new LL
+            // reverse it ans store in new LL
+            // compare with original  O(n) and space= O(n)
+        
+        // Approach 2;
+        if (head == NULL || head->next == NULL) {
+            return true; 
         }
 
-        int left = 0;
-        int right = arr.size() - 1;
+        // Step 1: Find the middle of the list
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast->next != nullptr && fast->next->next != nullptr) {
+            slow = slow->next;         
+            fast = fast->next->next;   
+        }
 
-        while (left < right) {
-            if (arr[left] != arr[right]) {
-                return false;
+        // Step 2: Reverse the second half
+        ListNode* secondHalf = reverseList(slow->next);
+
+        // Step 3: Compare both halves
+        ListNode* firstHalf = head;
+        ListNode* check = secondHalf;
+        while (check != nullptr) {
+            if (firstHalf->val != check->val) {
+                return false; 
             }
-            left++;
-            right--;
+            firstHalf = firstHalf->next;
+            check = check->next;
         }
-
-        return true;        
+        return true;
     }
 };
