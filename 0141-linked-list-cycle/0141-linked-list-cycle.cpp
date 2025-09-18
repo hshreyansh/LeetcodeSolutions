e011950092e12728bc0fe2345de00ab74f8b2d4d
Shreyansh Shukla <shreyansh.maholi@gmail.com>
2025-09-19 02:10:35 +0530

    Time: 12 ms (38.42%), Space: 11.8 MB (79.9%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -8,7 +8,16 @@
  */
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
-        
+    bool hasCycle(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
     }
 };
