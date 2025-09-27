425f9843fb0639ca7f02c8977c5fec2c2f6d564b
Shreyansh Shukla <shreyansh.maholi@gmail.com>
2025-09-27 16:00:37 +0530

    Time: 12 ms (85.4%), Space: 28.4 MB (75.18%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string result ;
+        int n = s.size();
+        for(int i=0; i<n; i++){
+
+            if(s[i] == '*'){
+
+                result.pop_back();
+
+            }
+
+            else{
+                result.push_back(s[i]);
+            }
+        }
+
+        return result;
+    }
+};
