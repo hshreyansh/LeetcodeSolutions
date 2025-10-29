8be8ff2f9b129f4686ea5731826513e5fd76b238
Shreyansh Shukla <shreyansh.maholi@gmail.com>
2025-10-29 23:18:54 +0530

    Time: 0 ms (100%), Space: 8.2 MB (60.8%) - LeetHub

---
+++ 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int smallestNumber(int n) {
+        int result=1;
+        while(result<n){
+            result = 2*result+1;
+        }
+        return result;
+    }
+};
