76b7c5bb57a44c6d57825e9120e28ef936740edd
Shreyansh Shukla <shreyansh.maholi@gmail.com>
2025-12-15 01:08:39 +0530

    Time: 0 ms (100%), Space: 7.9 MB (50.83%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==1) return true;
-        if(n%2==0){
-            return true;
+        if (n <= 0) return false;
+        if(n==1 ) return true;
+        if(n%2==1){
+            return false;
         }
-        return false;
+        return isPowerOfTwo(n/2);
     }
 };
