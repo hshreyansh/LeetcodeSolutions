812e4324e5126b88a54fefcc77d77716f6b246b1
Shreyansh Shukla <shreyansh.maholi@gmail.com>
2026-01-09 23:20:14 +0530

    Time: 4 ms (65.35%), Space: 13.7 MB (48.82%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string ans="";
+        int i=0;
+        for(int i=0; i<s.length(); i++)
+        {
+            if(ans.length()>0 && ans[ans.length()-1] == s[i])
+            {
+                ans.pop_back();
+            }
+            else ans.push_back(s[i]);
+        }
+        return ans;
+    }
+};
