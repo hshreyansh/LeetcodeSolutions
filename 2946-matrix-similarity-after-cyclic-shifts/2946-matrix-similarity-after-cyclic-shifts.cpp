bb5904f944782f7c7a27f395d5e99f7c44c38221
Shreyansh Shukla <shreyansh.maholi@gmail.com>
2026-03-27 20:52:00 +0530

    Time: 0 ms (100%), Space: 31.2 MB (91.3%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -3,10 +3,20 @@ public:
     bool areSimilar(vector<vector<int>>& mat, int k) {
         int n = mat.size();
         int m = mat[0].size();
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                int idx = (j+k)%m;
-                if(mat[i][j] != mat[i][idx]) return false;
+        k %= m;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+
+                if(i % 2 == 0){ 
+                    if(mat[i][j] != mat[i][(j + k) % m])
+                        return false;
+                }
+                else{
+                    if(mat[i][j] != mat[i][(j - k + m) % m])
+                        return false;
+                }
+
             }
         }
         return true;
