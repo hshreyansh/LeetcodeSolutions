7dda5fc9c18fecd1c1863c819a1230b56ea71fe8
Shreyansh Shukla <shreyansh.maholi@gmail.com>
2026-03-30 16:46:35 +0530

    Time: 3 ms (11.11%), Space: 25.7 MB (77.78%) - LeetHub

--- 3880-minimum-absolute-difference-between-two-values/3880-minimum-absolute-difference-between-two-values.cpp
+++ 3880-minimum-absolute-difference-between-two-values/3880-minimum-absolute-difference-between-two-values.cpp
@@ -4,12 +4,13 @@ public:
         int n= nums.size();
         if(n==1) return -1;
         int miniDif = INT_MAX;
+        int one=-1;
+        int two=-1;
         for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                if( (nums[i]==1 && nums[j]==2) || (nums[i]==2 && nums[j]==1)){
-                    int dif  = abs(i - j);
-                    miniDif = min(dif, miniDif);
-                }
+            if(nums[i]==1) one=i;
+            else if(nums[i]==2) two=i;
+            if(one!=-1 & two!= -1){
+                miniDif = min(miniDif, abs(one-two));
             }
         }
         if(miniDif == INT_MAX) return -1;
