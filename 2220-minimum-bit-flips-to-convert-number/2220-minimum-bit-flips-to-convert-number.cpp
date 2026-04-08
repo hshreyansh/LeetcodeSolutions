5d8b984fe664d775367bf3bf9cb055d6d0666249
Shreyansh Shukla <shreyansh.maholi@gmail.com>
2026-04-09 01:24:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int count = 0;
-        while (start > 0 || goal > 0) {
-            if ((start & 1) != (goal & 1)) {
-                count++;
-            }
-            start >>= 1;
-            goal >>= 1;
+        int ans = 0;
+        int cnt = 0;
+
+        int diff = start ^ goal;
+        while(diff > 0){
+            ans = diff % 2;
+            if(ans % 2 == 1) cnt++;
+            diff = diff / 2;
         }
-        return count;
+
+        return cnt;
     }
 };
