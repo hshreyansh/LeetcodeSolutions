0b7ae4f3b034978ef74b09c2758d80018b61074d
Shreyansh Shukla <shreyansh.maholi@gmail.com>
2026-04-30 14:30:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -4,15 +4,38 @@ public:
         int n = s1.length();
         int m = s2.length();
 
+        // If s1 is larger than s2, no permutation can exist
         if (n > m) return false;
-        string sorted_s1 = s1;
-        sort(sorted_s1.begin(), sorted_s1.end());
 
-        for (int i = 0; i <= m - n; i++) {
+        // Frequency vectors for s1 and the current window in s2
+        vector<int> s1_freq(26, 0);
+        vector<int> s2_freq(26, 0);
 
-            string temp = s2.substr(i, n);
-            sort(temp.begin(), temp.end());
-            if (temp == sorted_s1) return true;
+        // Fill frequency of characters in s1
+        for (int i = 0; i < n; i++) {
+            s1_freq[s1[i] - 'a']++;
+        }
+
+        // Slide the window over s2
+        int i = 0; // left index of the sliding window
+        int j = 0; // right index of the sliding window
+        while (j < m) {
+            // Include a new character from the right end of the window
+            s2_freq[s2[j] - 'a']++;
+
+            // Check if the current window size matches the size of s1
+            if (j - i + 1 > n) {
+                // If we have passed the size of s1, we need to remove the leftmost character
+                s2_freq[s2[i] - 'a']--;
+                i++;
+            }
+
+            // Check if the current window's frequency matches s1's frequency
+            if (s1_freq == s2_freq) {
+                return true;
+            }
+
+            j++;
         }
 
         return false;
