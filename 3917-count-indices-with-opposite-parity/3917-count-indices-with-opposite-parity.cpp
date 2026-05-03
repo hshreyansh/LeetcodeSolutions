15a87a3d34d54b6e88d9781b4159caca01ea4843
Shreyansh Shukla <shreyansh.maholi@gmail.com>
2026-05-04 00:47:05 +0530

    Time: 0 ms (100%), Space: 32.6 MB (96.11%) - LeetHub

--- 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.cpp
+++ 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     vector<int> countOppositeParity(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>ans;
-        
-        for(int i=0; i<n; i++){
-            int evencount=0;
-            int oddcount=0;
-            if(nums[i]%2==0){
-                for(int j=i+1; j<n; j++){
-                    if(nums[j]%2!=0){
-                        oddcount++;
-                    }
-                }
-                ans.push_back(oddcount);
-            }
-            else{
-                for(int j=i+1; j<n; j++){
-                    if(nums[j]%2==0){
-                        evencount++;
-                    }
-                }
-                ans.push_back(evencount);    
+        int n = nums.size();
+        vector<int> ans(n);
+
+        int even = 0, odd = 0;
+
+        for(int i = n - 1; i >= 0; i--) {
+            if(nums[i] % 2 == 0) {
+                ans[i] = odd;
+                even++;
+            } else {
+                ans[i] = even;
+                odd++;
             }
         }
+
         return ans;
     }
 };
