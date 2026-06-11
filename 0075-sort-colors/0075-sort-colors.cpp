89b9536409c734309bfa26798d6df0164a8e6483
Shreyansh Shukla <shreyansh.maholi@gmail.com>
2026-06-12 01:54:19 +0530

    Time: 0 ms (100%), Space: 11.6 MB (47.6%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,10 +1,33 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        return sort(nums.begin(), nums.end());
-        // int zeroes, ones, twos;
-        // for(int i=0; i<nums.size(); i++){
-        //     if()
-        // }
+        int zeroes=0, ones=0, twos=0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==0){
+                zeroes++;
+            }
+            else if(nums[i]==1){
+                ones++;
+            }
+            else{
+                twos++;
+            }
+        }
+        int i=0;
+        while(zeroes){
+            nums[i]=0;
+            i++;
+            zeroes--;
+        }
+        while(ones){
+            nums[i]=1;
+            i++;
+            ones--;
+        }
+        while(twos){
+            nums[i]=2;
+            i++;
+            twos--;
+        }
     }
 };
