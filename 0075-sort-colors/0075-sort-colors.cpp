01dc0f722827aa58019a7476543230b07afbf71e
Shreyansh Shukla <shreyansh.maholi@gmail.com>
2026-06-12 01:41:14 +0530

    Time: 0 ms (100%), Space: 11.7 MB (47.6%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        return sort(nums.begin(), nums.end());
+        // int zeroes, ones, twos;
+        // for(int i=0; i<nums.size(); i++){
+        //     if()
+        // }
+    }
+};
